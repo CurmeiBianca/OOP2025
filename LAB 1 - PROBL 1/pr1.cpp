03a5d55a865159d10c553468c92164800a46ba2d
CurmeiBianca <bia.bianca.curmei@gmail.com>
2025-02-28 17:25:26 +0200

    problema_1

---
+++ LAB 1 - PROBL 1/pr1.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int conversie(const char* c)
+{
+	int rez = 0;
+	int semn = 1;
+
+	//in cazul in care avem numere negative
+	if (*c == '-')
+	{
+		semn = -1;
+		c++;
+	}
+
+	//convertim fiecare caracter intr-un numar
+	while (*c != '\0')
+	{
+		if (isdigit(*c)) //verificam daca avem un numar
+		{
+			rez = rez * 10 + (*c - '0'); //construim rezultatul
+		}
+		else
+		{
+			break;
+		}
+		c++;
+	}
+
+	return rez * semn; //returnam numarul construit
+}
+
+int main()
+{
+	FILE* file;
+	char line[256];
+	int suma = 0;
+	errno_t err;
+
+	//Deschidem fisierul folosind fopen_s
+	err = fopen_s(&file, "C:\\Users\\HP\\source\\repos\\problema 1\\x64\\Debug\\in.txt", "r");
+	if (err != 0)
+	{
+		printf("Eroare: Nu s-a putut deschide fisierul.\n");
+		return 1;
+	}
+
+	//Citim fiecare linie din fisier si modificam suma
+	while (fgets(line, sizeof(line), file))
+	{
+		suma = suma + conversie(line);
+	}
+
+	//Inchidem fisierul
+	fclose(file);
+
+	//Afisam suma nmerelor
+	printf("Suma numerelor este: %d\n", suma);
+
+	return 0;
+}
