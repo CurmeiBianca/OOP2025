c71c77f27baafbf8d6bc880ad4635af103c04d50
CurmeiBianca <bia.bianca.curmei@gmail.com>
2025-03-04 15:00:09 +0200

    problema_2

--- LAB 1 - PROBL 2/pr2.cpp
+++ LAB 1 - PROBL 2/pr2.cpp
@@ -1,60 +1,56 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
+#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 
-//Functie pentru compararea a doua cuvinte
-int comparare(const void* a, const void* b)
+int comparare(char* c1, char* c2)
 {
-	//Conversia pointer-ilor la pointeri de tip char
-	char* word1 = *(char**)a;
-	char* word2 = *(char**)b;
-
-	//Compara lungimea cuvintelor (in ordine descrescatoare)
-	int lung1 = strlen(word1);
-	int lung2 = strlen(word2);
+	int lung1 = strlen(c1);
+	int lung2 = strlen(c2);
 
 	if (lung1 != lung2)
-		return lung2 - lung1; //Sorteaza dupa lungime, de la cel mai lung la cel mai scurt
+		return lung1 > lung2;
 
-	//Daca lungimile sunt egale, compara alfabetic 
-	return strcmp(word1, word2);
+	return strcmp(c1, c2) > 0;
+}
+
+void sortare(char cuvinte[][100], int nrcuv)
+{
+	for (int i = 0; i < nrcuv - 1; i ++)
+		for (int j = i + 1; j < nrcuv; j ++)
+			if (comparare(cuvinte[i], cuvinte[j]) == 0)
+			{
+				char aux[100];
+				strcpy_s(aux, sizeof(aux), cuvinte[i]);
+				strcpy_s(cuvinte[i], sizeof(cuvinte[i]), cuvinte[j]);
+				strcpy_s(cuvinte[j], sizeof(cuvinte[j]), aux);
+			}
 }
 
 int main()
 {
 	char input[1000];
-	char* words[100];
+	char cuvinte[100][100];
 	int nrcuv = 0;
 
-	printf("Introdu o propozitie:");
-	fgets(input, sizeof(input), stdin); //stdin - fluxul standard de intrare (tastatura)
+	printf("Introdu o propozitie: ");
+	fgets(input, sizeof(input), stdin);
 
-	//Elimina caracterul de newline daca este prezent
-	input[strcspn(input, "\n")] = '\0';
+	input[strlen(input) - 1] = NULL;
 
-	//Contextul pentru strtok_s
 	char* context = NULL;
 
-	//Imparte propozitia in cuvinte
-	char* cuvant = strtok_s(input, " \t\r\n", &context); //" \t\r\n -> setul de caractere de delimitatori; spatiul, tab-ul, caracterul de carriage return, caracterul de newline"
-	while (cuvant != NULL)
+	char* p = strtok_s(input, " ", &context);
+	while (p != NULL)
 	{
-		words[nrcuv++] = cuvant;
-		cuvant = strtok_s(NULL, " \t\r\n", &context); //se foloseste context (un pointer) pentru a sti de unde sa continuam
+		strcpy_s(cuvinte[nrcuv], sizeof(cuvinte[nrcuv]), p);
+		nrcuv++;
+		p = strtok_s(NULL, " ", &context);
 	}
 
-	//Sorteaza cuvintele folosind qsort si functia de comparare personalizata 
-	qsort(words, nrcuv, sizeof(char*), comparare); //qsort -> folosita pentru a sorta un tablou de siruri de caractere (words)
+	sortare(cuvinte, nrcuv);
 
-	//Afiseaza cuvintele sortate
 	printf("Cuvintele sortate: \n");
 	for (int i = 0; i < nrcuv; i++)
-	{
-		printf("%s\n", words[i]);
-	}
+		printf("%s \n", cuvinte[i]);
 
 	return 0;
 }
