2614a383e88924e3f0854d23610afb606264e522
CurmeiBianca <bia.bianca.curmei@gmail.com>
2025-03-10 19:02:09 +0200

    functii.h

---
+++ LAB 2 - PROBL 2/functii.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "student.h"
+
+//Functiile globale pentru comparari
+int compareByName(student* obj1, student* obj2);
+int compareByMathGrade(student* obj1, student* obj2);
+int compareByEnglishGrade(student* obj1, student* obj2);
+int compareByHistoryGrade(student* obj1, student* obj2);
+int compareByAverageGrade(student* obj1, student* obj2);
+
